a93892f6d06feb8cba09246fda148ad29db43bd2
pu5hp <pushpdeeepsinghv@gmail.com>
2024-06-01 13:13:30 +0530

    added three testing

---
+++ simple-App-DB-deep-mock-5/src/__mocks__/db.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@prisma/client'
+import { beforeEach } from 'vitest'
+import { mockDeep, mockReset } from 'vitest-mock-extended'
+
+export const prismaClient = mockDeep<PrismaClient>()
+
+//mocks need to be right next to mocks which should be right next to that fileb
